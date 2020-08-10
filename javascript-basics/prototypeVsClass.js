148abc09695d7059ebe667d85cf072b2e6fba8a2
vignesh <vigneshit13@gmail.com>
2020-08-10 06:18:13 +0530

    Prototypal inheritance added

--- javascript-basics/prototypeVsClass.js
+++ javascript-basics/prototypeVsClass.js
@@ -21,11 +21,39 @@ let EmployeeA = class extends Person {
     this.id = id;
     this.salary = salary;
   }
+
+  employeeInfo() {
+    return `Name : ${this.name} :: Id: ${this.id} :: Salary: ${this.salary}`;
+  }
 }
 
 const employee = new EmployeeA('bob', 123, 15000);
 console.log('From Employee Object: ', employee.getDetails());
-
+console.log('---------------------------------------------------')
 //  Prototype
 
-// 9840228776
+let PPerson = function(name, id) {
+  this.name = name;
+  this.id = id;
+}
+
+PPerson.prototype.getDetails = function() {
+  return `Name : ${this.name} :: Id: ${this.id}`;
+}
+
+const person1 = new PPerson('Foo', 123);
+console.log(person1.getDetails());
+
+let PEmployee = function(name, id, salary) {
+  PPerson.call(this, name, id);
+  this.salary = salary;
+}
+
+Object.setPrototypeOf(PEmployee, PPerson.prototype);
+
+PEmployee.prototype.employeeInfo = function() {
+  return `Name : ${this.name} :: Id: ${this.id} :: Salary: ${this.salary}`;
+}
+
+const personObj = new PEmployee('Bar', 123, 123345)
+console.log(personObj.employeeInfo());
